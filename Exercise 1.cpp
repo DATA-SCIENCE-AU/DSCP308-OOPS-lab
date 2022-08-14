289183e6f96052f7184f30b3361077085d8fda7a
Rajadurai balamurugan <106241420+Rajadurai2@users.noreply.github.com>
2022-08-14 07:23:41 +0530

    C++

--- Exercise 1.cpp
+++ Exercise 1.cpp
@@ -4,39 +4,39 @@
 class Distance
 {
 private:
-int feet;
-float inches;
+  int feet;
+  float inches;
 
 
 public:
-void setdist(int ft, float in)
-{
-feet = ft; inches = in;
-}
+  void setdist(int ft, float in)
+  {
+    feet = ft; inches = in;
+  }
 
-void getdist()
-{
-cout << "\nEnter feet: "; cin >> feet;
-cout << "Enter inches: "; cin >> inches;
-}
+  void getdist()
+  {
+    cout << "\nEnter feet: "; cin >> feet;
+    cout << "Enter inches: "; cin >> inches;
+  }
 
-void showdist()
-{
-cout << feet << "\'-" << inches << "\" ";
-}
+  void showdist()
+  {
+    cout << feet << "\'-" << inches << "\" ";
+  }
 };
 
 int main()
 {
-Distance dist1, dist2; 
-clrscr();
-dist1.setdist(11, 6.25);
-dist2.getdist(); 
-clrscr();
-cout << "\ndist1 = ";
-dist1.showdist(); 
-cout << "\ndist2 = "; 
-dist2.showdist(); 
-getch();
-return 0;
+  Distance dist1, dist2; 
+  clrscr();
+  dist1.setdist(11, 6.25);
+  dist2.getdist(); 
+  clrscr();
+  cout << "\ndist1 = ";
+  dist1.showdist(); 
+  cout << "\ndist2 = "; 
+  dist2.showdist(); 
+  getch();
+  return 0;
 }
