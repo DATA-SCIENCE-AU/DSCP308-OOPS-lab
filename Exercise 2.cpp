02f2409ed0806bb657b6f309f9363c8adfcd52b2
Rajadurai balamurugan <106241420+Rajadurai2@users.noreply.github.com>
2022-08-22 10:10:27 -0700

    C++

--- Exercise 2.cpp
+++ Exercise 2.cpp
@@ -1,40 +1,43 @@
 #include <iostream.h>
 #include <conio.h>
 
-class Counter
+class Counter     // Creating a Class Counter 
 {
 private:
-unsigned int count; public:
-Counter() : count(0)
-{ }
+    unsigned int count;     
 
-Counter(int c)
+public:
+
+// Creating Constructors
+Counter():
+count(0)                        // 1.Do nothing constructor
+{ 
+
+}
+Counter(int c)                 // 2. Parameter constructor
 {
 count = c;
 }
-
-Counter (Counter &c)
+Counter (Counter &c)          // 3. Copy  Constructor
 {
 count = c.count;
 }
 
-void inc_count()
+void inc_count()              // creating the method inc_count in the classs
 {
-
-
 count++;
 }
 
-void dec()
+void dec()                    // creating the method dec
 {
 count--;
 }
 
-void show()
+void show()                   // creating the method show
 {
 cout<<"Count is "<<count<<endl;
 }
-
+// Creating destructors
 ~Counter()
 {
 cout<<"Object destroyed\n";
@@ -43,10 +46,14 @@ cout<<"Object destroyed\n";
 
 int main()
 {
-Counter c1, c2(20),c3(c2); clrscr();
-c1.inc_count(); c2.inc_count(); c3.dec();
+Counter c1, c2(20),c3(c2);           // Initialise the Class Counter
+clrscr();       
+c1.inc_count();                    // call  the class method using . operator
+c2.inc_count(); 
+c3.dec();
 c1.show();
 c2.show();
 c3.show();
-getch(); return 0;
+getch();
+return 0;
 }
